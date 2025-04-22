e7e6a720f7677cd6e14d72975d48c814047f0e61
garveyshah <garveyshah1595@gmail.com>
2025-04-22 04:07:45 +0300

    Remove unused Instant import from main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use std::time::Duration;
-use std::time::Instant;
 
 use rand::Rng;
 use road_intersection::constants::{WINDOW_HEIGHT, WINDOW_WIDTH};
